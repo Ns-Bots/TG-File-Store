7c14bb415be8cf44d40330514c63d0f2cea04a14
AnoNymouS1033 <73804229+AnoNymouS1033@users.noreply.github.com>
2021-05-20 09:38:43 +0530

    Update Forcesub.py

--- plugins/Forcesub.py
+++ plugins/Forcesub.py
@@ -18,7 +18,7 @@ async def forcesub(c, m):
                return
         except UserNotParticipant:
             buttons = [[InlineKeyboardButton(text='Updates Channel 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
-            if m.text:
+            if m.command:
                 if (len(m.command) > 1) & (m.command[0] == 'start'):
                     chat_id, msg_id = m.command[1].split('_')
                     buttons.append([InlineKeyboardButton('🔄 Refresh', callback_data=f'refresh+{chat_id}+{msg_id}')])
