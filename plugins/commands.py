9dfd7c71a756b54fab3f91106060089812014a9a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-23 19:45:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,7 +110,7 @@ async def batch(c, m):
 
     while m.from_user.id in BATCH:
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Done ✅', callback_data='done')]])
-        media = await c.ask(chat_id=m.from_user.id, 'Ok 😉. Now send me some more files Or press done to get shareable link.', reply_markup=reply_markup)
+        media = await c.ask(chat_id=m.from_user.id, text='Ok 😉. Now send me some more files Or press done to get shareable link.', reply_markup=reply_markup)
         files.append(media)
 
     string = ""
