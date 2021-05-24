3c9d8658329f722a83a883b8b8316a0814ecf020
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-24 09:40:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -143,4 +143,4 @@ async def batch(c, m):
     bot = await c.get_me()
     url = f"https://t.me/{bot.username}?start=batch_{m.chat.id}_{string}" if not DB_CHANNEL_ID else f"https://t.me/{bot.username}?start=batch_{m.chat.id}_{string}"
 
-    await media.edit(text=url)
+    await media.request.edit(text=url)
