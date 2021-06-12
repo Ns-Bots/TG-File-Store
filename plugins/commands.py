6c7885ab79cc4c274a4a17b09929c4829ea6bac0
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-06-12 15:48:09 +0530

    Removed uploader details at caption  (#9)

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,26 +85,7 @@ async def start(c, m, cb=False):
         if msg.empty:
             return await send_msg.edit(f"🥴 Sorry bro your file was deleted by file owner or bot owner\n\nFor more help contact my owner 👉 {owner.mention(style='md')}")
         
-        caption = f"{msg.caption.markdown}\n\n\n" if msg.caption else ""
-
-        if chat_id.startswith('-100'): #if file from channel
-            channel = await c.get_chat(int(chat_id))
-            caption += "**--Uploader Details:--**\n\n"
-            caption += f"__📢 Channel Name:__ `{channel.title}`\n\n"
-            caption += f"__🗣 User Name:__ @{channel.username}\n\n" if channel.username else ""
-            caption += f"__👤 Channel Id:__ `{channel.id}`\n\n"
-            caption += f"__💬 DC ID:__ {channel.dc_id}\n\n" if channel.dc_id else ""
-            caption += f"__👁 Members Count:__ {channel.members_count}\n\n" if channel.members_count else ""
-
-        else: #if file not from channel
-            user = await c.get_users(int(chat_id))
-            caption += "**--Uploader Details:--**\n\n"
-            caption += f"__🦚 First Name:__ `{user.first_name}`\n\n"
-            caption += f"__🐧 Last Name:__ `{user.last_name}`\n\n" if user.last_name else ""
-            caption += f"__👁 User Name:__ @{user.username}\n\n" if user.username else ""
-            caption += f"__👤 User Id:__ `{user.id}`\n\n"
-            caption += f"__💬 DC ID:__ {user.dc_id}\n\n" if user.dc_id else ""
-
+        caption = f"{msg.caption.markdown}" if msg.caption else ""
         await send_msg.delete()
         await msg.copy(m.from_user.id, caption=caption)
 
