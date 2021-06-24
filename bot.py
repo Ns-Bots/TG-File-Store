f952dcd6035266ab1d0abcb6511f0ea6132544c0
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-06-24 11:58:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,10 +8,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyromod import listen
 from pyrogram import Client
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-
+from config import *
 
 def main():
     plugins = dict(root="plugins")
