04f67fc90ff484ddcbd193609a30203b25a3b188
Srujan-q123 <87603868+Srujan-q123@users.noreply.github.com>
2021-07-18 16:59:51 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -70,11 +70,11 @@ async def about_cb(c, m):
 
 👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
 
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [SRUJAN'S BOT](https://t.me/srujan_ka_bots)
 
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+👥 𝐆𝐫𝐨𝐮𝐩: [SRUJAN'S BOTS SUPPORT GROUP](https://t.me/Ns_Bot_supporters)
 
-🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/Ns-Bots/TG-File-Store)
+🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://t.me/srujan_ka_bots)
 """
 
     # creating buttons
