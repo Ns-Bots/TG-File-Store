6812ad6c1f880e55e4b43eb57591f32499b2d265
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-08-07 10:33:59 +0530

    Bug fixing

--- plugins/Forcesub.py
+++ plugins/Forcesub.py
@@ -19,7 +19,7 @@ async def forcesub(c, m):
             buttons = [[InlineKeyboardButton(text='Updates Channel 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
             if m.text:
                 if (len(m.text.split()) > 1) & ('start' in m.text):
-                    decoded_data = await decode(m.command[1])
+                    decoded_data = await decode(m.text.split()[1])
                     chat_id, msg_id = decoded_data.split('_')
                     buttons.append([InlineKeyboardButton('🔄 Refresh', callback_data=f'refresh+{chat_id}+{msg_id}')])
             await m.reply_text(
