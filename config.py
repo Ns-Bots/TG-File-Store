eb1b7c3e11184b61ccf9d3c54c64da17108dd2de
mjwebhacks <98449007+mjwebhacks@users.noreply.github.com>
2022-07-11 09:59:22 +0530

    Added Protected Content Support (#28)
    * Added Protected Content
    
    * Added Protected Content Support

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID"))
+PROTECT_CONTENT = True
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
